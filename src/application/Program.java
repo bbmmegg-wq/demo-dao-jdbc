f3ec3eed2cea0a5c0504b2b8114346af678aaa4f
Beatriz Moreira <bbmmegg@gmail.com>
2026-01-16 11:54:54 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,28 @@
 package application;
 
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.Scanner;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "books");
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
 		
-		System.out.println(obj);
+		Department department= new Department();
+		
+		Seller seller= new Seller(21, "Bob", "Bob@gmail.com", LocalDate.now(), 3000.00, department);
+		
+		System.out.println(seller);
+		
+		
+		sc.close();
+	
 	}
 
 }
